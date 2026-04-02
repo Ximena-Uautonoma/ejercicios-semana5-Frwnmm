56e219282d74b745ddb73f7f5d1b0304b7d4b118
Frwnmm <francisco.lira@cloud.uautonoma.cl>
2026-04-02 18:52:38 +0000

    Repondi el erecicio 2

--- Semana5/Ejercicio2.py
+++ Semana5/Ejercicio2.py
@@ -5,9 +5,12 @@ Un sistema de climatización clasifica:
 "Calurosa": más de 25
 Solicita la temperatura e indica la clasificación correspondiente.
 '''
-
-
-
-
-
-
+a = int(input("Ingresa un la temperatura actual en grados C y te dire si es fria, templada o calurosa: "))
+if a <= 10:
+    print("Afuera esta Frio")
+elif a <= 25:
+    print("Afuera esta Templado")
+elif a > 25:
+    print("Afuera esta soleado")
+else:
+    print(")
