49fb2561ed378ba489c5cae7364d547f9f6db55e
Frwnmm <francisco.lira@cloud.uautonoma.cl>
2026-04-02 19:46:08 +0000

    Terminadoo

--- Semana5/Ejercicio4.py
+++ Semana5/Ejercicio4.py
@@ -5,5 +5,9 @@ Solicita repetidamente:
 0 para terminar
 Al final, muestra cuántos días asistió.'''
 
-
-
+sum= 0
+i = 1
+while i == 1:
+    sum= sum + i
+    i = int(input("Ya es otro dia, 1 si asististe, 0 si no asisitiste: "))
+    print("Hoy asististe, has asistido",sum, "dias")
